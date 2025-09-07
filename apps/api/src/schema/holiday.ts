d037e34b50d3297b4868ded08f4bdb9f3d95106a
Balint Ipkovich <ipko1996@gmail.com>
2025-09-07 10:27:09 +0200

    feat: add holiday relations to schema; establish relationship with restaurant

--- apps/api/src/schema/holiday.ts
+++ apps/api/src/schema/holiday.ts
@@ -1,3 +1,4 @@
+import { relations } from 'drizzle-orm';
 import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 
 import { restaurant } from './restaurant';
@@ -9,3 +10,10 @@ export const holiday = pgTable('holiday', {
     .notNull()
     .references(() => restaurant.id),
 });
+
+export const holidayRelations = relations(holiday, ({ one }) => ({
+  restaurant: one(restaurant, {
+    fields: [holiday.restaurantId],
+    references: [restaurant.id],
+  }),
+}));
